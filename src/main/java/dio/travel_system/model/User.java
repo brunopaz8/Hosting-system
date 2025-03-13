214d680f34c76dcbf9c525e0bbe2a53c76358dd0
bruno <bp.nascimento12@gmail.com>
2025-03-13 14:23:28 -0300

    inicialização do projeto

---
+++ src/main/java/dio/travel_system/model/User.java
@@ -0,0 +1,23 @@
+package dio.travel_system.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+     
+    @Column(nullable = false, length = 100)
+    private String name;
+    
+
+    
+
+}
---
+++ src/test/java/dio/travel_system/TravelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.travel_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
