f506e36807dc103a2331648530f69e876d35f40e
bruno <bp.nascimento12@gmail.com>
2025-03-15 19:13:27 -0300

    correção de sintaxe

--- src/main/java/dio/travel_system/controller/HostingController.java
+++ src/main/java/dio/travel_system/controller/HostingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,7 +51,7 @@ public class HostingController {
         return ResponseEntity.created(location).body(hostingCreated);
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Hosting> update(@PathVariable long id, @Valid @RequestBody Hosting hostingToUpdate) {
         hostingToUpdate.setId(id);
         var hostingUpdated = hostingService.update(hostingToUpdate);
--- src/main/java/dio/travel_system/controller/UserController.java
+++ src/main/java/dio/travel_system/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +52,7 @@ public class UserController {
         return ResponseEntity.created(location).body(userCreated);
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<User> update(@PathVariable Long id, @Valid @RequestBody User userToUpdate) {
         userToUpdate.setId(id);
         var userUpdated = userService.update(userToUpdate);
