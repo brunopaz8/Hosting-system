b2d25d88ca29eef74ce40157def0db928e329c49
bruno <bp.nascimento12@gmail.com>
2025-04-01 16:04:54 -0300

    Handler Update

--- src/main/java/dio/travel_system/Repositories/HostingRepository.java
+++ src/main/java/dio/travel_system/Repositories/HostingRepository.java
@@ -1,7 +1,7 @@
 package dio.travel_system.repositories;
 
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +11,6 @@ import dio.travel_system.model.Hosting;
 @Repository
 public interface HostingRepository extends JpaRepository<Hosting, Long> {
     
-    List<Hosting> findByCity(String city);
+    
    
 }
